295867c7ebd1d9ba1a08d854d09bc3eb85aa15e2
MarialiceH <137061846+MarialiceH@users.noreply.github.com>
2023-07-11 16:35:01 -0400

    Inital commit of loops and arrays in JavaScript

---
+++ script.js
@@ -0,0 +1,31 @@
+//js code here
+var employeeAges = [15, 36, 78, 25, 17, 42, 19, 18];
+var age = employeeAges.indexOf(36);
+
+console.log(age)
+
+
+
+
+
+
+
+
+
+
+//var timeOfDay = [6, "noon", 8, "morning", "evening", 12];
+
+//timeOfDay.splice(4, 0, "twighlight", 2, 9);
+//timeOfDay.pop();
+
+
+
+
+
+//var meals = []
+//timeOfDay[3] = "midnight";
+
+//console.log(timeOfDay);
+
+//console.log(timeOfDay.includes("morning"));
+//console.log(timeOfDay.includes(9));
